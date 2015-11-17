71048375710be308f6f69239cfc5df41aeb376af
NicolasBulte <nicolas.bulte@ugent.be>
2015-11-17 09:42:40 +0100

    Update cmgTuples_Phys14_signals.py
    Added colors

--- samples/python/cmgTuples_Phys14_signals.py
+++ samples/python/cmgTuples_Phys14_signals.py
@@ -1,3 +1,5 @@
+import ROOT
+
 allSignalStrings=[\
 "SMS_T2tt_2J_mStop425_mLSP325",
 "SMS_T2tt_2J_mStop500_mLSP325",
@@ -20,3 +22,8 @@ for s in allSignalStrings:
   sm = getSignalSample(s)
   exec(s+"=sm")
   exec("allSignals.append(s)")
+
+SMS_T2tt_2J_mStop425_mLSP325['color'] = ROOT.kRed
+SMS_T2tt_2J_mStop500_mLSP325['color'] = ROOT.kBlue
+SMS_T2tt_2J_mStop650_mLSP325['color'] = ROOT.kGreen
+SMS_T2tt_2J_mStop850_mLSP100['color'] = ROOT.kMagenta
