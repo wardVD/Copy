26b7e47f90328ce67d2d8ca58fd58439074eeba0
Ward Van Driessche <wardvandriessche@gmail.com>
2015-12-01 15:26:09 +0100

    complete top pt reweighting

--- tools/python/topPtReweighting.py
+++ tools/python/topPtReweighting.py
@@ -1,15 +1,18 @@
-def getUnscaledTopPtReweightungFunction(a,b)
+from math import exp
 
-  return f(pt1)
+def getUnscaledTopPtReweightungFunction(a,b):
+  def f(pt1):
+    return exp(a+b*pt1)
+  return f
 
-def getUnscaledTopPairPtReweightungFunction(a,b)
-  return f(pt1,pt2)
+def getUnscaledTopPairPtReweightungFunction(a,b):
+  def f(pt1,pt2):
+    return exp(2*a+b*(pt1+pt2))
+  return f
 
 def getTopPtDrawString(a,b):
-  return s
-
-
-
+#  assert b < 0, "b is not a negative number"
+  return "exp(Sum$(("+str(a)+"+("+str(b)+")*genPartAll_pt)*(abs(genPartAll_pdgId)==6&&genPartAll_nDaughters==2&&genPartAll_status==62)))"
 ##### c.Draw(getTopPtDrawString(a,b)+">>h")
 ##### average=ROOT.h.GetMean()
 ##### topPairPtWeight = getUnscaledTopPairPtReweightungFunction(a,b)(pt1,pt2)/average
