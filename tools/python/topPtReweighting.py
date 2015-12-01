a5517d1cbd7adf16602a3fc4e92c7c5746f71a97
schoefbeck <robert.schoefbeck@cern.ch>
2015-12-01 17:11:44 +0100

    top pt fixes

--- tools/python/topPtReweighting.py
+++ tools/python/topPtReweighting.py
@@ -5,7 +5,11 @@ from math import exp, sqrt
 #    return exp(a+b*pt1)
 #  return f
 
-def getUnscaledTopPairPtReweightungFunction(a=0.148, b=-0.00129):
+#https://indico.cern.ch/event/463433/contribution/5/attachments/1193635/1733505/MSavitskyi_ToppTRew13TeV_TopModGen.pdf
+aDef = 0.148
+bDef = -0.00129
+
+def getUnscaledTopPairPtReweightungFunction(a=aDef, b=bDef):
   def f(topPts):
     assert len(topPts)<=2, "Found too many top pts: %i"%len(topPts)
     res=1.
@@ -15,7 +19,7 @@ def getUnscaledTopPairPtReweightungFunction(a=0.148, b=-0.00129):
   return f
 
 #https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
-def getTopPtDrawString(a=0.148, b= -0.00129): 
+def getTopPtDrawString(a=aDef, b=bDef): 
 #  assert b < 0, "b is not a negative number"
   return "sqrt(exp(Sum$(("+str(a)+"+("+str(b)+")*genPartAll_pt)*(abs(genPartAll_pdgId)==6&&genPartAll_nDaughters==2&&genPartAll_status==62&&genPartAll_pt<=400))))"
 
