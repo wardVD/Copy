b23957929c5e38d4be159f151e40ebb8869cb97b
schoefbeck <robert.schoefbeck@cern.ch>
2015-12-01 16:18:39 +0100

    fix sqrt

--- tools/python/topPtReweighting.py
+++ tools/python/topPtReweighting.py
@@ -1,18 +1,17 @@
-from math import exp
+from math import exp, sqrt
 
-def getUnscaledTopPtReweightungFunction(a,b):
-  def f(pt1):
-    return exp(a+b*pt1)
-  return f
+#def getUnscaledTopPtReweightungFunction(a,b):
+#  def f(pt1):
+#    return exp(a+b*pt1)
+#  return f
 
 def getUnscaledTopPairPtReweightungFunction(a,b):
   def f(pt1,pt2):
-    return exp(2*a+b*(pt1+pt2))
+    return sqrt(exp(2*a+b*(pt1+pt2)))
   return f
 
-def getTopPtDrawString(a,b):
+#https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
+def getTopPtDrawString(a=0.148, b= -0.00129): 
 #  assert b < 0, "b is not a negative number"
   return "sqrt(exp(Sum$(("+str(a)+"+("+str(b)+")*genPartAll_pt)*(abs(genPartAll_pdgId)==6&&genPartAll_nDaughters==2&&genPartAll_status==62&&genPartAll_pt<=400))))"
-##### c.Draw(getTopPtDrawString(a,b)+">>h")
-##### average=ROOT.h.GetMean()
-##### topPairPtWeight = getUnscaledTopPairPtReweightungFunction(a,b)(pt1,pt2)/average
+
