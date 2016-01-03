ec6ea78031a53f08447c57fdd4584a01d910546f
schoefbeck <robert.schoefbeck@cern.ch>
2016-01-03 18:54:25 +0100

    resolved

--- tools/python/localInfo.py
+++ tools/python/localInfo.py
@@ -2,15 +2,8 @@ import os
 if os.environ['USER'] in ['wvandrie']:
   #plotDir = "/afs/cern.ch/user/w/wvandrie/www/Stops/"
   plotDir = "/user/wvandrie/public_html/"
-<<<<<<< HEAD
-  #dataDir = "/afs/cern.ch/work/w/wvandrie/public/STOPS/ANALYSIS_new2/CMSSW_7_4_12_patch4/src/StopsDilepton/samplesCopyWard_Spring15_new/"
-  #dataDir = "/afs/cern.ch/work/w/wvandrie/public/STOPS/ANALYSIS_new2/CMSSW_7_4_12_patch4/src/StopsDilepton/samplesCopyWard_Spring15_mAOD/"
-  dataDir = "/user/wvandrie/STOPS/ANALYSIS/CMSSW_7_4_12_patch4/src/mAODv2_tiny/"
-  #dataDir = "/afs/hephy.at/data/rschoefbeck01/cmgTuples/postProcessed_mAODv2/dilep/"
   analysisOutputDir = '.' #Path to analysis results
-=======
   dataDir = "/user/wvandrie/STOPS/ANALYSIS/CMSSW_7_4_12_patch4/src/tuples_2015_12_2/dilepTiny/"
->>>>>>> c2099514cf17f7f9f64ea9c4711aa02c99f2c962
 if os.environ['USER'] in ['didar']:
   plotDir = "."
   dataDir = "~/eos/cms/store/cmst3/group/susy/schoef/postProcessed_Phys14V3_diLep" #needs EOS mount on lxplus at ~/eos 
