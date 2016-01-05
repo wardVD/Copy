7a419cf309c37e756218a38aad9548265fea9087
Ward Van Driessche <wardvandriessche@gmail.com>
2016-01-05 16:57:28 +0100

    rewrite with output root files

--- plots/plotsWard/TTZStudy/TTZ_study.py
+++ plots/plotsWard/TTZStudy/TTZ_study.py
@@ -1,4 +1,4 @@
-import ROOT
+import ROOT, glob
 ROOT.gROOT.LoadMacro("$CMSSW_BASE/src/StopsDilepton/tools/scripts/tdrstyle.C")
 ROOT.setTDRStyle()
 import numpy as n
@@ -18,14 +18,6 @@ import collections
 #puReweightingFunc = getReweightingFunction(era="doubleMu_onZ_isOS_1500pb_nVert_reweight")
 #puReweighting = lambda c:puReweightingFunc(getVarValue(c, "nVert"))
 
-"""
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("--njets", dest="njets", default=2, type=int, action="store", help="Select minimal amount of jets")
-parser.add_option("--nbjets", dest="nbjets", default=1, type=int, action="store", help="Select minimal amount of b-tagged jets")
-
-(options, args) = parser.parse_args()
-"""
 
 import sys
 
@@ -42,7 +34,7 @@ from StopsDilepton.samples.cmgTuples_Data25ns_mAODv2_postProcessed import *
 #######################################################
 #        SELECT WHAT YOU WANT TO DO HERE              #
 #######################################################
-reduceStat = 1 #recude the statistics, i.e. 10 is ten times less samples to look at
+reduceStat = 100 #recude the statistics, i.e. 10 is ten times less samples to look at
 makedraw1D = True
 makeTexFile = True
 mt2llcutscaling = False
@@ -59,21 +51,21 @@ presel_nbjet       = 'Sum$(Jet_pt>30&&abs(Jet_eta)<2.4&&Jet_id&&Jet_btagCSV>'+st
 presel_nlep        = '(nGoodElectrons+nGoodMuons)>=2&&Sum$(LepGood_pt>10)>=2'
 presel_triggers    = '(HLT_mumuIso||HLT_ee_DZ||HLT_mue)'
 
-data = [DoubleMuon_Run2015D,DoubleEG_Run2015D,MuonEG_Run2015D]
-
-luminosity = data[0]["lumi"]
+luminosity = DoubleMuon_Run2015D["lumi"]
 
 datacut = "(Flag_HBHENoiseFilter&&Flag_goodVertices&&Flag_CSCTightHaloFilter&&Flag_eeBadScFilter&&weight>0)"
 
 preselection = presel_njet+'&&'+presel_nbjet+'&&'+presel_nlep+'&&'+presel_triggers
 
-backgrounds = [DY_HT_LO,TTJets,WJetsToLNu,singleTop,QCD_Mu5,TTZ,TTW,TZQ,TTH,diBoson]
-#backgrounds = [TTZ,TTJets]
+exec('sample=['+sys.argv[3]+']')
+
+#backgrounds = [DY_HT_LO,TTJets,WJetsToLNu,singleTop,QCD_Mu5,TTZ,TTW,TZQ,TTH,diBoson]
+#backgrounds = [TTZ]
 
 #######################################################
 #            get the TChains for each sample          #
 #######################################################
-for s in backgrounds+data:
+for s in sample:
   s['chain'] = getChain(s,histname="")
 
 mt2llbinning = [15,0,300]
@@ -152,7 +144,7 @@ plots = {\
 #######################################################
 #            Start filling in the histograms          #
 #######################################################
-for s in backgrounds+data:
+for s in sample:
   #construct 1D histograms
   for lepton in plots.keys():
     for plot in plots[lepton].keys():
@@ -162,13 +154,13 @@ for s in backgrounds+data:
   chain = s["chain"]
 
   if (s == DoubleMuon_Run2015D): 
-    eList = getEList(chain, preselection) if not s['isData'] else getEList(chain, preselection+'&&'+datacut+'&&HLT_mumuIso')
+    eList =  getEList(chain, preselection+'&&'+datacut+'&&HLT_mumuIso')
   elif (s == DoubleEG_Run2015D):
-    eList = getEList(chain, preselection) if not s['isData'] else getEList(chain, preselection+'&&'+datacut+'&&HLT_ee_DZ')
+    eList =  getEList(chain, preselection+'&&'+datacut+'&&HLT_ee_DZ')
   elif (s == MuonEG_Run2015D):   
-    eList = getEList(chain, preselection) if not s['isData'] else getEList(chain, preselection+'&&'+datacut+'&&HLT_mue')
+    eList =  getEList(chain, preselection+'&&'+datacut+'&&HLT_mue')
   else:
-    eList = getEList(chain, preselection) if not s['isData'] else getEList(chain, preselection)
+    eList = getEList(chain, preselection)
   
   nEvents = eList.GetN()/reduceStat
   print "Found %i events in %s after preselection %s, looping over %i" % (eList.GetN(),s["name"],preselection,nEvents)
@@ -231,8 +223,6 @@ for s in backgrounds+data:
               Z_pt = diLep.Pt()
               zleptons = [leptons[Lep1],leptons[Lep2]]
 
-    ttleptons = [lep for lep in leptons if lep not in zleptons]
-              
 
     if abs(mZ - 91.2) < 15: #there is a dilepton pair from a Z
       if len(leptons)==2:
@@ -312,161 +302,23 @@ for s in backgrounds+data:
       for i in range(nbinsx):
         if plots[lepton][plot]['histo'][s['name']].GetBinContent(i+1) < 0: plots[lepton][plot]['histo'][s['name']].SetBinContent(i+1,0.)
 
-
-print "listofdata: ", len(ListOfDataEvents)
         
 processtime = datetime.now()
 print "Time to process chains: ", processtime - start
-
-
-for lepton in plots.keys():
-  for plot in plots[lepton].keys():
-    totalbkg = 0
-    dataint = 0
-    for b in backgrounds:
-      totalbkg += plots[lepton][plot]['histo'][b["name"]].Integral()
-    for d in data:
-      dataint += plots[lepton][plot]['histo'][d["name"]].Integral()
-
-    for b in backgrounds:
-      if (noscaling or dataint == 0):
-        plots[lepton][plot]['SF'] = 1.
-      else:
-        plots[lepton][plot]['histo'][b["name"]].Scale(dataint/totalbkg)
-        plots[lepton][plot]['SF'] = dataint/totalbkg
 
 
 #######################################################
 #             Output text file                        #
 #######################################################
 
-output = open('./yields/yields_njet_'+njetscut[1]+'_nbjet_'+nbjetscut[1]+'.txt','w')
-for lepton in sorted(plots.keys()):
-  error = n.zeros(1,dtype=float)
-  output.write(lepton + '\n')
-  output.write('on Z \n')
-  for b in sorted(backgrounds,key=lambda sort:plots[lepton]['dl_mass_onZ']['histo'][sort['name']].Integral()):
-    output.write(' %-*s : %s +- %s' % (20,b['name'],str(plots[lepton]['dl_mass_onZ']['histo'][b['name']].IntegralAndError(1,mllbinning[0],error)),error[0]))
-    output.write('\n')
-  for d in sorted(data,key=lambda sort:plots[lepton]['dl_mass_onZ']['histo'][sort['name']].Integral()):
-    output.write(' %-*s : %s +- %s' % (20,d['name'],str(plots[lepton]['dl_mass_onZ']['histo'][d['name']].IntegralAndError(1,mllbinning[0],error)),error[0]))
-    output.write('\n')
-  output.write(' \n off Z \n')
-  for b in sorted(backgrounds,key=lambda sort:plots[lepton]['dl_mass_offZ']['histo'][sort['name']].Integral()):
-    output.write(' %-*s : %s +- %s' % (20,b['name'],str(plots[lepton]['dl_mass_offZ']['histo'][b['name']].IntegralAndError(1,mllbinning[0],error)),error[0]))
-    output.write('\n')
-  for d in sorted(data,key=lambda sort:plots[lepton]['dl_mass_offZ']['histo'][sort['name']].Integral()):
-    output.write(' %-*s : %s +- %s' % (20,d['name'],str(plots[lepton]['dl_mass_offZ']['histo'][d['name']].IntegralAndError(1,mllbinning[0],error)),error[0]))
-    output.write('\n')
-  output.write('\n')
-
-#######################################################
-#             Drawing done here                       #
-#######################################################
-
-legendtextsize = 0.032
-a=[]
-double = n.zeros(1, dtype=float)
-#ROOT.gStyle.SetErrorX(0.5)
-histopad =  [0.0, 0.2, 1.0, .95]
-datamcpad = [0.0, 0.0, 1.0, 0.2]
-lumitagpos = [0.4,0.95,0.6,1.0]
-channeltagpos = [0.45,0.8,0.6,0.85]
-legendpos = [0.6,0.6,0.95,1.0]
-scalepos = [0.8,0.95,0.95,1.0]
-
-if makedraw1D:
-  for lepton in plots.keys():
+for lepton in plots.keys():
+  for s in sample:
+    rootfilepath = "./OutputRootFiles/"+lepton+'_njet_'+njetscut[1]+'_nbjet_'+nbjetscut[1]+'/data/' if s['isData'] else "./OutputRootFiles/"+lepton+'_njet_'+njetscut[1]+'_nbjet_'+nbjetscut[1]+'/backgrounds/'
+    if not os.path.exists(rootfilepath): os.makedirs(rootfilepath)
+    rootfile = ROOT.TFile(rootfilepath+s['name']+".root","recreate")
     for plot in plots[lepton].keys():
-      l=ROOT.TLegend(legendpos[0],legendpos[1],legendpos[2],legendpos[3])
-      a.append(l)
-      l.SetFillColor(0)
-      l.SetShadowColor(ROOT.kWhite)
-      l.SetBorderSize(1)
-      l.SetTextSize(legendtextsize)
-
-      bkg_stack = ROOT.THStack("bkgs","bkgs") 
-      totalbackground = plots[lepton][plot]['histo'][backgrounds[0]['name']].Clone()
-      totaldata = plots[lepton][plot]['histo'][data[0]['name']].Clone()
-      for b in backgrounds:
-        if b!= backgrounds[0]:totalbackground.Add(plots[lepton][plot]['histo'][b['name']])
-      for d in data:
-        if d!= data[0]:totaldata.Add(plots[lepton][plot]['histo'][d['name']])
-
-      for j,b in enumerate(sorted(backgrounds,key=lambda sort:plots[lepton][plot]["histo"][sort["name"]].Integral())):
-        plots[lepton][plot]['histo'][b["name"]].SetMarkerSize(0)
-        plots[lepton][plot]['histo'][b["name"]].SetFillColor(b["color"])
-        plots[lepton][plot]['histo'][b["name"]].SetLineWidth(1)
-        bkg_stack.Add(plots[lepton][plot]['histo'][b["name"]],"h")
-        l.AddEntry(plots[lepton][plot]['histo'][b["name"]],b['texName'],"f")
-
-      c1 = ROOT.TCanvas("c1","c1",800,800)
-      pad1 = ROOT.TPad("","",histopad[0],histopad[1],histopad[2],histopad[3])
-      a.append(pad1)
-      pad1.SetBottomMargin(0)
-      pad1.SetTopMargin(0)
-      pad1.SetRightMargin(0.05)
-      pad1.Draw()
-      pad1.cd()
-      pad1.SetLogy()
-      totaldata.Draw("pe1same")
-      bkg_stack.Draw("same")
-      totaldata.Draw("pe1same")
-      bkg_stack.GetXaxis().SetLabelSize(0.)
-      l.Draw()
-      ROOT.gPad.RedrawAxis()
-
-      totaldata.GetXaxis().SetTitle(plots[lepton][plot]['title'])
-      totaldata.GetYaxis().SetTitle("Events (A.U.)")
-      totaldata.GetYaxis().SetRangeUser(0.005,1000000)
-      l.AddEntry(totaldata,'data')
-
-      lumitag = ROOT.TPaveText(lumitagpos[0],lumitagpos[1],lumitagpos[2],lumitagpos[3],"NDC")
-      scaletag = ROOT.TPaveText(scalepos[0],scalepos[1],scalepos[2],scalepos[3],"NDC")
-      lumitag.AddText("lumi: "+str(luminosity)+' pb^{-1}')
-      scaletag.AddText("Scale Factor: " +str(round(plots[lepton][plot]['SF'],2)))
-      lumitag.SetFillColor(ROOT.kWhite)
-      lumitag.SetShadowColor(ROOT.kWhite)
-      lumitag.SetBorderSize(0)
-      scaletag.SetShadowColor(ROOT.kWhite)
-      scaletag.SetFillColor(ROOT.kWhite)
-      scaletag.SetBorderSize(0)
-      c1.cd()
-      pad2 = ROOT.TPad("","",datamcpad[0],datamcpad[1],datamcpad[2],datamcpad[3])
-      a.append(pad2)
-      pad2.SetGrid()
-      pad2.SetBottomMargin(0.4)
-      pad2.SetTopMargin(0)
-      pad2.SetRightMargin(0.05)
-      pad2.Draw()
-      pad2.cd()
-      ratio = totaldata.Clone()
-      a.append(ratio)
-      ratio.Divide(totalbackground)
-      ratio.SetMarkerStyle(20)
-      ratio.GetYaxis().SetTitle("Data/Bkg.")
-      #ratio.GetYaxis().SetNdivisions(502)
-      ratio.GetXaxis().SetTitle(plots[lepton][plot]['title'])
-      ratio.GetXaxis().SetTitleSize(0.2)
-      ratio.GetYaxis().SetTitleSize(0.18)
-      ratio.GetYaxis().SetTitleOffset(0.29)
-      ratio.GetXaxis().SetTitleOffset(0.8)
-      ratio.GetYaxis().SetLabelSize(0.1)
-      ratio.GetXaxis().SetLabelSize(0.18)
-      ratio.SetMinimum(0)
-      ratio.SetMaximum(3)
-      ratio.Draw("pe")
-      c1.cd()
-      lumitag.Draw()
-      scaletag.Draw()
-      path = plotDir+'/test/TTZstudy/'+lepton+'_njet_'+njetscut[1]+'_nbjet_'+nbjetscut[1]+'/'
-      if not os.path.exists(path): os.makedirs(path)
-      c1.Print(path+plot+".png")
-      del ratio
-      del pad1
-      del pad2
-      c1.Clear()
-      
+      plots[lepton][plot]['histo'][s['name']].Write()
+    rootfile.Close()
 
 makeplotstime = datetime.now()
 
