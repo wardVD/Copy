85863564f835537652c4c4f310675bf5c5a613e8
Ward Van Driessche <wardvandriessche@gmail.com>
2016-01-11 11:03:14 +0100

    adding TTZ region

--- analysis/python/run_estimates.py
+++ analysis/python/run_estimates.py
@@ -1,12 +1,16 @@
-from setup import setup, regions, estimates
+from setup import setup, regions, regionTTZ, estimates, estimateTTZ
 
 setup.verbose = True
-for channel in ['EE']:
-  for r in regions[:1]:
+for channel in ['MuMu' ,'EE', 'EMu']:
+  for r in regions:
     for e in estimates:
       res = e.cachedEstimate(r, channel, setup)
       #jer = e.JERSystematic(r, channel, setup)
       #jec = e.JECSystematic(r, channel, setup)
-      print "\n Result", channel, e.name, r, res#, 'jer',jer, 'jec', jec
+      print "\n Result in ", channel," for estimate ", e.name, r,":", res#, 'jer',jer, 'jec', jec
       print "\n"
     print
+
+for channel in ['MuMu','EE','EMu']:
+  res = estimateTTZ.cachedEstimate(regionTTZ[0],channel,setup)
+  print "\n Result in ", channel," for estimate ", estimateTTZ.name, regionTTZ[0],":", res#, 'jer',jer, 'jec', jec
